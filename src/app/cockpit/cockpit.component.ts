9a749b9c88fbd34a792a8feed6268d9db6c0d5e5
Ajay kumar <ajay.krbca@gmail.com>
2020-05-07 10:00:34 +0530

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  newServerName:string;
+  newServerContent:string;
+  serverElements:any = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(){
+    this.serverElements.push({
+      type:'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBluePrint(){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+    
+
+}
